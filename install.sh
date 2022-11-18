5efa89e438c8c437d37d528c972f6baa154d5ef8
Srikrishna <srikrishna.s@hm.com>
2022-11-18 19:23:23 +0530

    progress

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ echo "Starting BASH SHELL"
 SCRIPT_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Script Root DIR: "$SCRIPT_ROOT_DIR
 echo "As User: "$USER
+cd ~
+pwd;
 echo ""
 
 
