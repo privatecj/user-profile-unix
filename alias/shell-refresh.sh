e5c3df10d1112f2eec611d998f12fda3bba8ba85
Srikrishna <srikrishna.s@hm.com>
2022-11-19 00:03:05 +0530

    progress

--- alias/shell-refresh.sh
+++ alias/shell-refresh.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-echo "Shell refresh"
-
 function download_profile(){
-    cd /home/$USER/mydrive;
-    pwd;
-    rm -rf user-profile
-    wget -O user-profile.zip https://www.dropbox.com/sh/p7gylx6vwupq5u3/AACcQSAO_EYXtJgRB_mHdP6ha?dl=1;
-    unzip -o user-profile.zip -d user-profile;
-    rm -f user-profile.zip
-    cd install;
-    find /home/$USER/mydrive/user-profile -type f -exec dos2unix {} \;
-    chown -R $USER /home/$USER
-    pwd
+    pushd ~/mydrive/user-profile
+    git pull;
+    popd
 }
 
 function install_profile(){
